1c6bb631bf484415bcc22ee26cb766c4dd5506bb
juano-cryp <ricojuancamilo27@gmail.com>
2025-03-06 20:53:13 -0500

    Add files via upload

---
+++ punto2.py
@@ -0,0 +1,9 @@
+#Entradas
+B = float(input("Ingrese la Base del rectangulo: ")) 
+A = float(input("Ingrese la Altura del rectangulo: ")) 
+
+#Caja negra
+Area= B * A
+
+#Salida
+print("El area del rectángulo es: " ,Area)
